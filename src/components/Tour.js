2b922ce493d4284b89494a34087e3066969fdbb9
Hoang Minh Thang <thangminhhoang7112002@gmail.com>
2024-05-14 09:36:40 +0700

    updated tours

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,26 +1,26 @@
-
-const Tour = ({icon, date, title, text, location, duration, cost}) => {
+const Tour = ({ icon, date, title, text, location, duration, cost }) => {
   return (
     <article className="tour-card">
-              <div className="tour-img-container">
-                <img src={icon} className="tour-img" alt="" />
-                <p className="tour-date">{date}</p>
-              </div>
-              <div className="tour-info">
-                <div className="tour-title">
-                  <h4>{title}</h4>
-                </div>
-                <p>
-                  {text}
-                </p>
-                <div className="tour-footer">
-                  <p>
-                    <span><i className="fas fa-map"></i></span> {location}
-                  </p>
-                  <p>{duration} days</p>
-                  <p>from ${cost}</p>
-                </div>
-              </div>
+      <div className="tour-img-container">
+        <img src={icon} className="tour-img" alt="" />
+        <p className="tour-date">{date}</p>
+      </div>
+      <div className="tour-info">
+        <div className="tour-title">
+          <h4>{title}</h4>
+        </div>
+        <p>{text}</p>
+        <div className="tour-footer">
+          <p>
+            <span>
+              <i className="fas fa-map"></i>
+            </span>{' '}
+            {location}
+          </p>
+          <p>{duration} days</p>
+          <p>from ${cost}</p>
+        </div>
+      </div>
     </article>
   )
 }
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,23 +1,18 @@
-import Title from "./Title"
-import Tour from "./Tour"
-import { tours } from "../data"
+import Title from './Title'
+import Tour from './Tour'
+import { tours } from '../data'
 
 const Tours = () => {
   return (
     <section className="section" id="tours">
-    <Title title='featured' subTitle='tours' />
-    <Tour />
+      <Title title="featured" subTitle="tours" />
 
-    <div className="section-center featured-center">
-      {
-        tours.map((tour) => {
-          return (
-            <Tour key={tour.id} {...tour}/>
-          )
-        })
-      }
-    </div>
-  </section>
+      <div className="section-center featured-center">
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />
+        })}
+      </div>
+    </section>
   )
 }
 export default Tours
